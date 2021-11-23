080e61637b8fa081603691a45ce1d40a901eae1a
ddesai2000 <ddesai22@amherst.edu>
2021-11-22 20:30:30 -0500

    added bar graph to shiny

--- ShinyTableTemplate.R
+++ ShinyTableTemplate.R
@@ -14,42 +14,25 @@ library(ggnetwork)
 library(igraph)
 library(shinythemes)
 
+# Import data
+platforms_all_genre_rating <- read_csv("platforms_all_genre_rating.csv")
 mtables <- read_csv("data/mtables.csv")
 
-#cb_choices <- unique(mtables$Service)
+# For TAB 1 Table widgets
+
+## For sliderInput choices for Year
 si_choices <- unique(mtables$Year)
+
+# For TAB 2 Bar Graph widgets:
+
+## For selectInput choices for Genre
+genre_choices <-  unique(platforms_all_genre_rating$Genres)
 
 ui <- navbarPage(
   title = "Who's Watching Netflix?", 
   theme = shinytheme("sandstone"),
   
-  # # Visualization 1: Time Series
-  # tabPanel(
-  #   title = "Salmon Abundance Per Year",
-  #   
-  #   sidebarLayout(
-  #     sidebarPanel(
-  #       
-  #       selectizeInput(inputId = "species",
-  #                      label = "Choose one or more species:",
-  #                      choices = ts_choices,
-  #                      selected = ts_choices,
-  #                      multiple = TRUE),
-  #       
-  #       
-  #       checkboxGroupInput(inputId = "age",
-  #                          label = "Select between maturity:",
-  #                          choices = age_choices,
-  #                          selected = age_choices,
-  #                          inline = TRUE)
-  #     ),
-  #     
-  #     mainPanel(plotOutput(outputId = "ts"))
-  #   )
-  # ),
-  
-  
-  # Visualization 2: Table
+  # Visualization 1: Table
   
   tabPanel(
     title = "Ratings, Ratings, Ratings",
@@ -61,22 +44,48 @@ ui <- navbarPage(
 #                      choices = cb_choices,
 #                      selected = "Netflix"),
         
-        sliderInput("yr", "Years:",min = min(si_choices), max = max(si_choices), value = c(1914, 2021),sep = "",),
-        sliderInput("imdb", "IMDb Rating:",min = 0, max = 10, value = c(0, 10),sep = "",),
-        sliderInput("rottentom", "Rotten Tomatoes Rating",min = 0, max = 100, value = c(0, 100),sep = "",),
+        sliderInput("yr", "Years:", min = min(si_choices), 
+                    max = max(si_choices), 
+                    value = c(1914, 2021),sep = "",),
+        sliderInput("imdb", "IMDb Rating:",min = 0, max = 10, value = c(0, 10), 
+                    sep = "",),
+        sliderInput("rottentom", "Rotten Tomatoes Rating",min = 0, max = 100, 
+                    value = c(0, 100),sep = "",),
       
       
       ),
       
-      
       mainPanel(DT::dataTableOutput(outputId = "table"))
+
+    )
+  ),
+
+  # Visualization 2: Bar Graph
+
+  tabPanel(
+    title = "Streaming Platform Competition",
+  
+    sidebarLayout(
+      sidebarPanel(
+      
+        # Create dropdown to allow user to select genre
+        selectInput(inputId = "genre_bar",
+                  label = "Select Genre:",
+                  choices = genre_choices,
+                  selected = "Crime"),
+        # Set width of this component
+        width = 3
+      ),
+      
+      mainPanel(
+        plotlyOutput(outputId = "bar"),
+      
+        # Set width of this component
+        width = 9
+      )
     )
   )
 )
-
-
-
-
 
 ############
 # server   #
@@ -84,37 +93,41 @@ ui <- navbarPage(
 
 server <- function(input, output){
   
-  # # Visualization 1: Time Series
-  # data_for_time <- reactive({
-  #   data <- filter(abundance, Age %in% input$age,
-  #                  Species %in% input$species)
-  # })
-  
-  # output$ts <- renderPlot({
-  #   ggplot(data = data_for_time(), aes(x = Year, y = Amount, color = Species,
-  #                                      linetype = Age)) +
-  #     geom_line(alpha = 0.7, stat = "identity") +
-  #     labs(title = "Salmon Abundance Per Year",
-  #          x = "Year",
-  #          y = "Total Number of Salmon (Millions)") +
-  #     scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
-  #     theme(plot.title = element_text(size = 20))
-  #   # +
-  #   #   scale_x_datetime(date_breaks = "1 year")
-  # })
-  
 
-  # Visualization 3
+  # Visualization 1: Table
 
   output$table <- DT::renderDataTable({
 #    addCheckboxButtons <- paste0('<input type="checkbox" name="row', cb_choices, '" Streaming Service="', cb_choices, '">',"")
 #    cbind(Pick=addCheckboxButtons, mtables[, input$streamingservice, drop=FALSE])
-    dt1 <- mtables[mtables$Year >= input$yr[1] & mtables$Year <= input$yr[2],] 
-    dt2 <- mtables[mtables$IMDb >= input$imdb[1] & mtables$IMDb <= input$imdb[2],] 
-    dt3 <- mtables[mtables$RottenTomatoes >= input$rottentom[1] & mtables$RottenTomatoes <= input$rottentom[2],] 
+    dt1 <- mtables[(mtables$Year >= input$yr[1] & mtables$Year <= input$yr[2]),] 
+    dt2 <- mtables[(mtables$IMDb >= input$imdb[1] & 
+                     mtables$IMDb <= input$imdb[2]),] 
+    dt3 <- mtables[(mtables$RottenTomatoes >= input$rottentom[1] & 
+                     mtables$RottenTomatoes <= input$rottentom[2]),] 
 
   })
   
+  # Visualization 2: Bar Graph
+  data_for_bar <- reactive({
+    # Filtering dataset for year and species based off the user input
+    data <- filter(platforms_all_genre_rating, Genres %in% input$genre_bar)
+  })
+  
+  output$bar <- renderPlotly({
+    # Create bar chart for Shows/Movies Average Rating by Platform
+    # For different genres
+    ggplotly(ggplot(data = data_for_bar(), 
+                    # Set Platform as x var and Average Rating as y var
+                    # Fill the bars with color by species
+                    aes(x = Service, y = IMDb)) +
+               geom_bar(alpha = 0.7, stat = "identity", fill = c("#FF6666")) +
+               # Label axes
+               labs(title = "Average IMDb Rating of Shows/Movies by Platform",
+                    x = "Platform",
+                    y = "IMDb Rating (Out of 10)"),
+             # Only show amount when hovering
+             tooltip = "IMDb") 
+  })
   
 }
 
@@ -125,4 +138,5 @@ server <- function(input, output){
 shinyApp(ui = ui, server = server)
 
 # # embed code into blog
-# knitr::include_app("https://your-account.shinyapps.io/your-app/", height = "600px")
+# knitr::include_app("https://your-account.shinyapps.io/your-app/", 
+# height = "600px")
